7dfcf3e0985ddbcf73915886df2e59e3aa553bbb
vinterdo <pio4426@gmail.com>
2017-06-30 01:22:45 +0200

    Nicer look, show more data

--- diagram.js
+++ diagram.js
@@ -12,14 +12,20 @@ function initTree(treeName, nodes) {
 
 // the template we defined earlier
     myDiagram.nodeTemplate =
-        $(go.Node, "Horizontal",
+        $(go.Node, "Vertical",
             {background: "#44CCFF"},
-            $(go.Picture,
-                {margin: 10, width: 50, height: 50, background: "red"},
-                new go.Binding("source")),
             $(go.TextBlock, "Default Text",
-                {margin: 12, stroke: "white", font: "bold 16px sans-serif"},
-                new go.Binding("text", "name"))
+                {margin: 5, stroke: "white", font: "bold 16px sans-serif"},
+                new go.Binding("text", "name")),
+            $(go.TextBlock, "Default Text",
+                {margin: 3, stroke: "white", font: "10px sans-serif"},
+                new go.Binding("text", "description")),
+            $(go.TextBlock, "Default Text",
+                {margin: 3, stroke: "white", font: "10px sans-serif"},
+                new go.Binding("text", "production")),
+            $(go.TextBlock, "Default Text",
+                {margin: 3, stroke: "white", font: "10px sans-serif"},
+                new go.Binding("text", "cost"))
         );
 
 // define a Link template that routes orthogonally, with no arrowhead
@@ -32,13 +38,5 @@ function initTree(treeName, nodes) {
 
     var model = $(go.TreeModel);
     model.nodeDataArray = nodes;
-        /*[
-            {key: "1", name: "Don Meow", source: "cat1.png"},
-            {key: "2", parent: "1", name: "Demeter", source: "cat2.png"},
-            {key: "3", parent: "1", name: "Copricat", source: "cat3.png"},
-            {key: "4", parent: "3", name: "Jellylorum", source: "cat4.png"},
-            {key: "5", parent: "3", name: "Alonzo", source: "cat5.png"},
-            {key: "6", parent: "2", name: "Munkustrap", source: "cat6.png"}
-        ];*/
     myDiagram.model = model;
 }
